a1502c2cf23aca76c6fd318655456fa0b4e8b258
lgw2238 <nihao3634@naver.com>
2023-03-08 14:42:43 +0900

    commit

---
+++ src/main/java/org/example/ChatGwAplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGwAplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ChatGwAplication.class, args);
+
+    }
+}
